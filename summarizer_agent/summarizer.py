5b30b4a596127744f952793fd367a853a68864b4
szaqal <malczyk.pawel@gmail.com>
2026-06-15 15:18:54 +0200

    feat: use Polish summarization prompt

--- summarizer_agent/summarizer.py
+++ summarizer_agent/summarizer.py
@@ -5,7 +5,7 @@ from langchain_mistralai import ChatMistralAI
 from summarizer_agent.config import MISTRAL_API_KEY
 
 _prompt = ChatPromptTemplate.from_template(
-    "Summarize the following article in 3-5 sentences:\n\n{text}"
+    "Streść poniższy artykuł w 3-5 zdaniach po polsku:\n\n{text}"
 )
 _llm = ChatMistralAI(model="mistral-small-latest", api_key=MISTRAL_API_KEY)
 _chain = _prompt | _llm | StrOutputParser()
